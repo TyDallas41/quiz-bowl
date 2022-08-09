fdb282badba9ef9f214f59a64beb95005bd40611
Ty the Guy <Tylerhodge15@gmail.com>
2022-08-09 10:24:46 -0500

    updated html and css

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const startButton = document.getElementById('start-btn')
+const questionBoxElement = document.getElementById('question-box')
+
+startButton.addEventListener('click', startGame)
+
+function startGame(){
+    questionBoxElement.classList.remove('hide')
+}
+
+function nextQuestion(){
+
+}
+
+function selectChoice(){
+
+}
